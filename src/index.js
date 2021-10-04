f9974920ef698665647ff3e33a4914521cc8ec23
marianagdeveloper <mariana.guanda@gmail.com>
2021-10-03 22:56:14 -0500

    MVP 1
    Code Javascript for Mariana Guanda

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+//-----------Diferent values---------------//
+
+// let creditCardNumber = '42345678'; //true
+let creditCardNumber = "4083952015263"; //false
+// let creditCardNumber = '1'; //false
+
+validator.isValid(creditCardNumber);
+
+validator.maskify(creditCardNumber);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,80 @@
+/* eslint-disable no-console */
 const validator = {
-  // ...
+  isValid(number) {
+    console.log(number);
+    let arrayInverse = [];
+    let arrayEvenOdd = [];
+    let arrayTotal = [];
+    let sNumber = number.toString();
+    let numberTemporary;
+    let numberTemporaryAdd = 0;
+    let numberTemporaryInt = 0;
+
+    //Array Reverse
+    arrayInverse = sNumber.split("").reverse();
+    console.log(arrayInverse);
+
+    //Number in Even Position: 2,4,6,8...
+    for (let index = 0; index < arrayInverse.length; index++) {
+      if (index % 2 != 0) {
+        const evenPosition = arrayInverse[index] * 2;
+        arrayEvenOdd[index] = evenPosition;
+      } else {
+        arrayEvenOdd[index] = arrayInverse[index];
+      }
+    }
+    console.log(arrayEvenOdd);
+
+    //Number is greater than 10
+    for (let index = 0; index < arrayEvenOdd.length; index++) {
+      if (arrayEvenOdd[index] > 9) {
+        arrayTotal[index] = arrayEvenOdd[index];
+        numberTemporary = arrayEvenOdd[index].toString().split("");
+        numberTemporary.forEach((element) => {
+          numberTemporaryInt = parseInt(element);
+          numberTemporaryAdd += numberTemporaryInt;
+        });
+        arrayTotal[index] = numberTemporaryAdd;
+        //restart values
+        numberTemporaryAdd = 0;
+        numberTemporaryInt = 0;
+      } else {
+        arrayTotal[index] = parseInt(arrayEvenOdd[index]);
+      }
+    }
+
+    //isValid
+    console.log(arrayTotal);
+    let result = 0;
+    arrayTotal.forEach((element) => {
+      result += element;
+    });
+    console.log(result);
+    return result % 10 == 0 ? true : false;
+
+    /*arrayInverse:  ['8', '7', '6', '5', '4', '3', '2', '1']
+       arrayEvenOdd: ['8', 14, '6', 10, '4', 6, '2', 2]
+       arrayTotal:   [8, 5, 6, 1, 4, 6, 2, 2]
+       result:       34 // false
+     */
+  },
+
+  maskify(number) {
+    let arrayMask = [];
+    let sNumber = number.toString();
+
+    //Replace #
+    for (let index = 0; index < sNumber.length; index++) {
+      if (index <= sNumber.length - 5) {
+        arrayMask[index] = "#";
+      } else {
+        arrayMask[index] = sNumber[index];
+      }
+    }
+    let result = arrayMask.join("");
+    console.log(result);
+    return result;
+  },
 };
 
 export default validator;
